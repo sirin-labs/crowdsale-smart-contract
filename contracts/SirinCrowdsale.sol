d9fc334cea07b0edee04be7397cf15aa5e21dd39
yaron.shlomo <yarons@sirinlabs.com>
2017-09-27 17:45:26 +0300

    issue srn to multisig wallets

--- contracts/SirinCrowdsale.sol
+++ contracts/SirinCrowdsale.sol
@@ -16,9 +16,8 @@ contract SirinCrowdsale is FinalizableCrowdsale {
     // Crowdsale override
     // =========================================
 
-
     // @return the crowdsale rate with bonus
-    function getRate() public returns (uint256) {
+    function getRate() internal returns (uint256) {
 
         //10% bonus within the first 24 hours
         uint firstStep = startTime + 24 hours;
@@ -44,9 +43,22 @@ contract SirinCrowdsale is FinalizableCrowdsale {
 
     function finalization() internal {
 
-        uint256 strategicPartnershipTokens = SafeMath.div(SafeMath.mul(token.totalSupply, 15),10);
+        uint256 totalSupply = token.totalSupply();
 
-        token.issue(0x00130f1b288ebB1dC157261146bb1cB9EF33192c, strategicPartnershipTokens);
+        //25% from totalSupply which is 10% of the total number of SRN tokens will be allocated to the founders and
+        //team and will be gradually vested over a 12-month period
+        token.issue(0x00803306C76b1bf476cdCe3b308e146Ee1D201B4, SafeMath.div(SafeMath.mul(totalSupply, 250),1000));
+
+        //25% from totalSupply which is 10% of the total number of SRN tokens will be allocated to OEM’s, Operating System implementation,
+        //SDK developers and rebate to device and Shield OS™ users
+        token.issue(0x00eB0E5f489d90455d7BF202fF1a638f510E276b, SafeMath.div(SafeMath.mul(totalSupply, 250),1000));
+
+        //12.5% from totalSupply which is 5% of the total number of SRN tokens will be allocated to professional fees and Bounties
+        token.issue(0x0059e796c5Bc1F15B9310a8461deBADDC8F8af21, SafeMath.div(SafeMath.mul(totalSupply, 125),1000));
+
+        //87.5% from totalSupply which is 35% of the total number of SRN tokens will be allocated to SIRIN LABS,
+        //and as a reserve for the company to be used for future strategic plans for the created ecosystem,
+        token.issue(0x00522Ea95d76DFC9AaA8A25B3DD33Ef2d5c86929, SafeMath.div(SafeMath.mul(totalSupply, 875),1000));
 
         // Re-enable transfers after the token sale.
         token.disableTransfers(false);
