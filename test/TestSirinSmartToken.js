bdf52bf2404a16aa22fc3ec628d45f2e178c5a05
yaron.shlomo <yarons@sirinlabs.com>
2017-10-15 11:01:43 +0300

    fix and add disable transfer buy default test

--- test/TestSirinSmartToken.js
+++ test/TestSirinSmartToken.js
@@ -1,11 +1,12 @@
-const StoxSmartToken = artifacts.require('../contracts/SirinSmartToken.sol');
+const SirinSmartToken = artifacts.require('../contracts/SirinSmartToken.sol');
+const utils = require('./helpers/Utils');
 
 contract('SirinSmartToken', (accounts) => {
     let token;
     let owner = accounts[0];
 
     beforeEach(async () => {
-        token = await StoxSmartToken.new();
+        token = await SirinSmartToken.new();
     });
 
     describe('construction', async () => {
@@ -28,5 +29,20 @@ contract('SirinSmartToken', (accounts) => {
         it('should be initialized as not transferable', async () => {
             assert.equal(await token.transfersEnabled(), false);
         });
+
+        it('should throw when attempting to transfer by default', async () => {
+            let token = await SirinSmartToken.new();
+            await token.issue(accounts[0], 1000);
+            let balance = await token.balanceOf.call(accounts[0]);
+            assert.equal(balance, 1000);
+
+            try {
+                await token.transfer(accounts[1], 100);
+                assert(false, "didn't throw");
+            }
+            catch (error) {
+                return utils.ensureException(error);
+            }
+         });
     });
 });
