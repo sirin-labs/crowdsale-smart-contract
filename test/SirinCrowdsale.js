a47b411d3af072f13330867b540438adf02523ff
SIRINMOBILE\yossig <yossig@sirinlabs.com>
2017-10-29 13:41:09 +0200

    fix the gateRate and add test

--- test/SirinCrowdsale.js
+++ test/SirinCrowdsale.js
@@ -25,7 +25,7 @@ contract('SirinCrowdsale', function ([_,investor, owner, wallet, walletFounder, 
   })
 
   beforeEach(async function () {
-    this.startTime = latestTime() + duration.weeks(1);;
+    this.startTime = latestTime() + duration.weeks(1);
     this.endTime =   this.startTime + duration.weeks(1)
     this.afterEndTime = this.endTime + duration.seconds(1)
 
@@ -47,14 +47,89 @@ contract('SirinCrowdsale', function ([_,investor, owner, wallet, walletFounder, 
       await increaseTimeTo(this.startTime)
     })
 
-    it('Rate first day ', async function () {
-
+    it('Should be on day 1 - 1000 ', async function () {
       let rate = await this.crowdsale.getRateMock.call()
-      console.log("Rate:" + rate);
-      assert.equal(rate, 500);
-    })
-  })
+      assert.equal(rate, 1000);
+    });
 
+    it('Should be on day 2 - 950 ', async function () {
+      await increaseTimeTo(this.startTime + duration.days(1));
+      let rate = await this.crowdsale.getRateMock.call()
+      assert.equal(rate, 950);
+    });
+
+    it('Should be on day 3 - 900 ', async function () {
+      await increaseTimeTo(this.startTime + duration.days(2));
+      let rate = await this.crowdsale.getRateMock.call()
+      assert.equal(rate, 900);
+    });
+
+    it('Should be on day 4 - 855 ', async function () {
+      await increaseTimeTo(this.startTime + duration.days(3));
+      let rate = await this.crowdsale.getRateMock.call()
+      assert.equal(rate, 855);
+    });
+
+    it('Should be on day 5 - 810 ', async function () {
+      await increaseTimeTo(this.startTime + duration.days(4));
+      let rate = await this.crowdsale.getRateMock.call()
+      assert.equal(rate, 810);
+    });
+
+    it('Should be on day 6 - 770 ', async function () {
+      await increaseTimeTo(this.startTime + duration.days(5));
+      let rate = await this.crowdsale.getRateMock.call()
+      assert.equal(rate, 770);
+    });
+
+    it('Should be on day 7 - 730 ', async function () {
+      await increaseTimeTo(this.startTime + duration.days(6));
+      let rate = await this.crowdsale.getRateMock.call()
+      assert.equal(rate, 730);
+    });
+
+    it('Should be on day 8 - 690 ', async function () {
+      await increaseTimeTo(this.startTime + duration.days(7));
+      let rate = await this.crowdsale.getRateMock.call()
+      assert.equal(rate, 690);
+    });
+
+    it('Should be on day 9 - 650 ', async function () {
+      await increaseTimeTo(this.startTime + duration.days(8));
+      let rate = await this.crowdsale.getRateMock.call()
+      assert.equal(rate, 650);
+    });
+
+    it('Should be on day 10 - 615 ', async function () {
+      await increaseTimeTo(this.startTime + duration.days(9));
+      let rate = await this.crowdsale.getRateMock.call()
+      assert.equal(rate, 615);
+    });
+
+    it('Should be on day 11 - 580 ', async function () {
+      await increaseTimeTo(this.startTime + duration.days(10));
+      let rate = await this.crowdsale.getRateMock.call()
+      assert.equal(rate, 580);
+    });
+
+    it('Should be on day 12 - 550 ', async function () {
+      await increaseTimeTo(this.startTime + duration.days(11));
+      let rate = await this.crowdsale.getRateMock.call()
+      assert.equal(rate, 550);
+    });
+
+    it('Should be on day 13 - 525 ', async function () {
+      await increaseTimeTo(this.startTime + duration.days(12));
+      let rate = await this.crowdsale.getRateMock.call()
+      assert.equal(rate, 525);
+    });
+
+    it('Should be on day 14 - 500 ', async function () {
+      await increaseTimeTo(this.startTime + duration.days(13));
+      let rate = await this.crowdsale.getRateMock.call()
+      assert.equal(rate, 500);
+    });
+  })
 
   describe('Finalize allocation', function () {
 
