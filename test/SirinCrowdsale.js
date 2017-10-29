8c71202e3ddcc5c76200e9c269cc62cfb55ef047
SIRINMOBILE\yossig <yossig@sirinlabs.com>
2017-10-29 09:52:25 +0200

    SirinCrowdsale fin test

---
+++ test/SirinCrowdsale.js
@@ -0,0 +1,64 @@
+import ether from './helpers/ether'
+import {advanceBlock} from './helpers/advanceToBlock'
+import {increaseTimeTo, duration} from './helpers/increaseTime'
+import latestTime from './helpers/latestTime'
+import EVMThrow from './helpers/EVMThrow'
+
+const BigNumber = web3.BigNumber
+
+const should = require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should()
+
+const SirinCrowdsale = artifacts.require('SirinCrowdsale.sol')
+const SirinSmartToken = artifacts.require('SirinSmartToken.sol')
+
+contract('SirinCrowdsale', function ([_,investor, owner, wallet, walletFounder, walletOEM, walletBounties, walletReserve]) {
+
+  const value = ether(1)
+
+  before(async function() {
+    //Advance to the next block to correctly read time in the solidity "now" function interpreted by testrpc
+    await advanceBlock()
+  })
+
+  beforeEach(async function () {
+    this.startTime = latestTime() + duration.weeks(1);;
+    this.endTime =   this.startTime + duration.weeks(1)
+    this.afterEndTime = this.endTime + duration.seconds(1)
+
+    this.crowdsale = await SirinCrowdsale.new(this.startTime,
+      this.endTime,
+      wallet,
+      walletFounder,
+      walletOEM,
+      walletBounties,
+      walletReserve,
+      {from: owner})
+
+    this.token = SirinSmartToken.at(await this.crowdsale.token())
+  })
+
+  describe('Finalize allocation', function () {
+
+    beforeEach(async function() {
+      await increaseTimeTo(this.startTime)
+    })
+
+    it('Allocate founder token amount as 10% of the total supply', async function () {
+
+      await this.crowdsale.sendTransaction({value: value, from: investor})
+
+      await increaseTimeTo(this.afterEndTime)
+      await this.crowdsale.finalize({from: owner})
+
+      const totalSupply = await this.token.totalSupply()
+      const expectedFounderTokenAmount =  totalSupply.div(10);
+      let walletFounderBalance = await this.token.balanceOf(walletFounder);
+
+      walletFounderBalance.should.be.bignumber.equal(expectedFounderTokenAmount);
+
+    })
+  })
+})
