cdcd954f6ffce644a5f98f08470159ad32728cd9
Leonid Beder <leonid@orbs.network>
2017-12-07 18:16:40 +0200

    Style.

--- test/helpers/FinalizableCrowdsaleMock.sol
+++ test/helpers/FinalizableCrowdsaleMock.sol
@@ -6,17 +6,13 @@ import '../../contracts/crowdsale/FinalizableCrowdsale.sol';
 
 contract FinalizableCrowdsaleMock is FinalizableCrowdsale {
 
+	function FinalizableCrowdsaleMock(uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet, SirinSmartToken _token) public
+  		Crowdsale(_startTime, _endTime, _rate, _wallet, _token)
+  		FinalizableCrowdsale()
+  	{
+  	}
 
-  function FinalizableCrowdsaleMock(uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet) public
-  Crowdsale(_startTime, _endTime, _rate, _wallet)
-  FinalizableCrowdsale()
-
-  {
-
-  }
-
-  function finalization() internal {
-    isFinalized = true;
-  }
-
+  	function finalization() internal {
+    	isFinalized = true;
+  	}
 }
